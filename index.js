27af7b65a0347bade701eaebb1c3d570b6fd3963
Jak Major <hellojakkaiser@gmail.com>
2021-09-01 10:41:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const driversObj = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+// findMatching- This function takes an array of drivers' names and a string as arguments, and returns the matching list of drivers. The function should be case insensitive.
+
+
+function findMatching(driverNamesArray, inputString){
+    return driverNamesArray.filter(driverName => driverName.toUpperCase() === inputString.toUpperCase())
+}
+
+function fuzzyMatch(driverNamesArray, inputString) {
+    return driverNamesArray.filter(driverName => driverName.charAt(0) === inputString.charAt(0))
+}
+
+function matchName(driverNamesObj, inputString) {
+    return driverNamesObj.filter(driverName => driverName.name === inputString)
+}
+
+
+//matchName - This function takes an array of driver objects and a string as arguments. Each driver object has two properties: name and hometown. The function should return each element whose name property matches the provided string argument.
